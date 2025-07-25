72503e84ae32168efdc3c83cfefa629d146fffe6
Doythan <won03289@gmail.com>
2025-07-25 17:24:05 +0900

    레포지토리 분리

--- security-app/src/main/resources/db/data.sql
+++
@@ -1,5 +0,0 @@
-insert into user_tb(roles, username, password, email)
-values('USER','ssar', '$2a$10$sZvrnV2FZO51MGrG2jTUU.zv3/K/vZFBW5MOYWPTkVeDeoZhH3rai', 'ssar@nate.com');
-
-insert into user_tb(roles, username, password, email)
-values('ADMIN,USER','cos', '$2a$10$sZvrnV2FZO51MGrG2jTUU.zv3/K/vZFBW5MOYWPTkVeDeoZhH3rai', 'ssar@nate.com');
