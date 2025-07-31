cab8787dba95d7ece81636e52585beb5fcf4363f
도경원 <ethan980824@naver.com>
2025-08-01 00:31:06 +0900

    게시판 CRUD 컨틀롤러 중심

--- springv1B/src/main/java/com/mtcoding/springv1b/controller/BoardController.java
+++ springv1B/src/main/java/com/mtcoding/springv1b/controller/BoardController.java
@@ -2,10 +2,13 @@ package com.mtcoding.springv1b.controller;
 
 import com.mtcoding.springv1b.controller.dto.BoardDetailResponseDTO;
 import com.mtcoding.springv1b.controller.dto.BoardListResponseDTO;
+import com.mtcoding.springv1b.controller.dto.BoardSaveRequestDTO;
+import com.mtcoding.springv1b.controller.dto.BoardUpdateRequestDTO;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,12 +22,16 @@ public class BoardController {
         List<BoardListResponseDTO> resDTO = new ArrayList<>();
         BoardListResponseDTO boardlist1 = new BoardListResponseDTO();
         BoardListResponseDTO boardlist2 = new BoardListResponseDTO();
+        BoardListResponseDTO boardlist3 = new BoardListResponseDTO();
         boardlist1.setId(1);
         boardlist1.setTitle("1번 제목");
         boardlist2.setId(2);
         boardlist2.setTitle("2번 제목");
+        boardlist3.setId(3);
+        boardlist3.setTitle("3번 제목");
         resDTO.add(boardlist1);
         resDTO.add(boardlist2);
+        resDTO.add(boardlist3);
         request.setAttribute("models", resDTO);
 
         return "board/list";
@@ -55,5 +62,20 @@ public class BoardController {
         resDTO.setContent("기존 내용");
         request.setAttribute("model", resDTO);
         return "board/update-form";
+    }
+
+    @PostMapping("/board/save")
+    public String save(BoardSaveRequestDTO reqDTO) {
+        return "redirect:/board";
+    }
+
+    @PostMapping("/board/{id}/delete")
+    public String deleteById(@PathVariable("id") int id) {
+        return "redirect:/board";
+    }
+
+    @PostMapping("/board/{id}/update")
+    public String updateById(BoardUpdateRequestDTO reqDTO, @PathVariable("id") int id) {
+        return "redirect:/board/" + id;
     }
 }
